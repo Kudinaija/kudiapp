c6063e1c186d151f6b8d974bc2ff6baad4d20cfb
Tob <b.timothy@handypros.ng>
2026-02-01 12:45:33 +0100

    Deployment test

--- src/main/java/com/kudiapp/kudiapp/services/PaymentService.java
+++ src/main/java/com/kudiapp/kudiapp/services/PaymentService.java
@@ -1,15 +1,18 @@
 package com.kudiapp.kudiapp.services;
 
+import com.kudiapp.kudiapp.dto.GenericResponse;
+import com.kudiapp.kudiapp.dto.request.payment.InitPaymentRequest;
+import com.kudiapp.kudiapp.dto.request.payment.InitPaymentResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface PaymentService {
 
-//    InitPaymentResponse initializePayment(InitPaymentRequest request);
-//
-//    GenericResponse verifyPayment(String reference);
-//
-//    boolean isSignatureValid(String rawBody, String signatureHeader);
-//    void processWebhookAsync(String rawBody);
+    InitPaymentResponse initializePayment(InitPaymentRequest request);
+
+    GenericResponse verifyPayment(String reference);
+
+    boolean isSignatureValid(String rawBody, String signatureHeader);
+    void processWebhookAsync(String rawBody);
 
 }
