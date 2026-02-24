05fcb16c1e00475f5666cc1aca8120e64d75b0bd
Tob <b.timothy@handypros.ng>
2026-02-24 23:41:30 +0100

    Plan creation date updated...

--- src/main/java/com/kudiapp/kudiapp/services/serviceImpl/productSeervice/ServiceProductPlanServiceImpl.java
+++ src/main/java/com/kudiapp/kudiapp/services/serviceImpl/productSeervice/ServiceProductPlanServiceImpl.java
@@ -23,6 +23,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -401,7 +402,9 @@ public class ServiceProductPlanServiceImpl implements ServiceProductPlanService 
         plan.setCurrency(request.getCurrency());
         plan.setPlanDescription(request.getPlanDescription());
         plan.setDisplayOrder(request.getDisplayOrder());
-        
+        plan.setCreatedAt(LocalDateTime.now());
+        plan.setUpdatedAt(LocalDateTime.now());
+
         if (request.getStatus() != null) {
             plan.setStatus(request.getStatus());
         } else if (plan.getStatus() == null) {
